e5ca4585e899947ae62f3e9dc2711665786b1f92
Tahrim19 <tarimbilal4@gmail.com>
2024-08-19 03:17:32 -0700

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home'>
+      hello nwjcnacl acjfwicm jbdf wejc jcbfem bvwe
+    </div>
+  )
+}
+
+export default Home
