72ce510b23ffb8a2556ebe852ad6ee716b496c4b
Tahrim19 <tarimbilal4@gmail.com>
2024-08-20 08:19:28 -0700

    nav-bar done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react'
-import {BrowserRouter as Router , Route , Routes} from 'react-router-dom'
-import Home from './components/Home'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <Router>
         <Routes>
-          <Route path = '/' element={<Home/>}/>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div className='home'>
-      hello nwjcnacl acjfwicm jbdf wejc jcbfem bvwe
+        hello
     </div>
   )
 }
