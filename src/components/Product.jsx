bbb95f26321b9acc29e42ad551c3519c7b78e19e
Tahrim19 <tarimbilal4@gmail.com>
2024-08-21 07:58:44 -0700

    add to cart functionality implemented

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,23 @@
 import '../css/product.css'
+import { useStateValue } from './StateProvider'
 
 export default function Product({id,title,image,price,rating}) {
+  const [{basket} , dispatch] = useStateValue()
+  
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item : {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    })
+    console.log("added to basket")
+  }
+
   return (
     <div className='product'>
       <div className='product-info'>
@@ -20,7 +37,7 @@ export default function Product({id,title,image,price,rating}) {
 
       </div>
       <img src={image} alt=""/>
-      <button>Add to Cart</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   )
 }
--- src/components/Reducer.jsx
+++ src/components/Reducer.jsx
@@ -12,6 +12,10 @@ export const getBasketTotal = (basket) => {
 const Reducer = (state,action) => {
     console.log(action)
     switch(action.type){
+        case "ADD_TO_BASKET":
+            return{ ...state , 
+                basket: [...state.basket , action.item],
+            }
         default:
             return state
     }
